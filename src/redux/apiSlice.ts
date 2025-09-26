8050735a03f245c61eaec0eabc3e4a9316f48478
xuding <xuding1@sinosafe.com.cn>
2025-09-26 15:56:09 +0800

    feat: 使用RTK Query编辑post

--- src/redux/apiSlice.ts
+++ src/redux/apiSlice.ts
@@ -9,10 +9,11 @@ export const apiSlice = createApi({
   endpoints: (builder) => ({
     getPosts: builder.query({
       query: () => '/posts',
-      providesTags: ['Post'],
+      providesTags: (result = []) => ['Post', ...result.map((item: any) => ({ type: 'Post', id: item.id }))],
     }),
     getPost: builder.query({
       query: (postId: string) => `/posts/${postId}`,
+      providesTags: (result, error, id) => [{ type: 'Post', id }],
     }),
     addNewPost: builder.mutation({
       query: (initialPost) => ({
@@ -22,7 +23,15 @@ export const apiSlice = createApi({
       }),
       invalidatesTags: ['Post'],
     }),
+    editPost: builder.mutation({
+      query: (post) => ({
+        url: `/posts/${post.id}`,
+        method: 'PATCH',
+        body: post,
+      }),
+      invalidatesTags: (result, error, arg) => [{ type: 'Post', id: arg.id }],
+    }),
   }),
 })
 
-export const { useGetPostsQuery, useGetPostQuery, useAddNewPostMutation } = apiSlice
+export const { useGetPostsQuery, useGetPostQuery, useAddNewPostMutation, useEditPostMutation } = apiSlice
--- src/view/EditPostForm.tsx
+++ src/view/EditPostForm.tsx
@@ -1,18 +1,76 @@
+// import { useState } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+// import { useParams, useNavigate } from 'react-router-dom'
+// import { toast } from 'react-tiny-toast'
+// import { RootState } from '../redux/store'
+// import { selectPostById, updatePost } from '../redux/posts'
+
+// export default function EditPostForm() {
+//   const { postId } = useParams()
+//   const post = useSelector((state: RootState) => selectPostById(state, postId || ''))
+
+//   const [title, setTitle] = useState(post ? post.title : '')
+//   const [content, setContent] = useState(post ? post.content : '')
+
+//   const dispatch = useDispatch()
+//   const navigate = useNavigate()
+
+//   function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
+//     setTitle(e.target.value)
+//   }
+//   function handleContentChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+//     setContent(e.target.value)
+//   }
+//   function handleSubmit(e: React.FormEvent<HTMLButtonElement>) {
+//     e.preventDefault()
+//     if (title && content) {
+//       dispatch(
+//         updatePost({
+//           id: postId,
+//           title,
+//           content,
+//         }),
+//       )
+//       setTitle('')
+//       setContent('')
+//       navigate(`/posts/${postId}`)
+//     } else {
+//       toast.show('Please fill in both fields', {
+//         variant: 'warning',
+//         position: 'top-center',
+//       })
+//     }
+//   }
+
+//   return (
+//     <section>
+//       <h2>Edit Post</h2>
+//       <form>
+//         <label htmlFor="postTitle">Post Title:</label>
+//         <input id="postTitle" name="postTitle" type="text" value={title} onChange={handleTitleChange} />
+//         <label htmlFor="postContent">Post Content:</label>
+//         <textarea id="postContent" name="postContent" value={content} onChange={handleContentChange} />
+//         <button type="submit" onClick={handleSubmit}>
+//           Save Post
+//         </button>
+//       </form>
+//     </section>
+//   )
+// }
+
 import { useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
 import { useParams, useNavigate } from 'react-router-dom'
 import { toast } from 'react-tiny-toast'
-import { RootState } from '../redux/store'
-import { selectPostById, updatePost } from '../redux/posts'
+import { useGetPostQuery, useEditPostMutation } from '../redux/apiSlice'
 
 export default function EditPostForm() {
   const { postId } = useParams()
-  const post = useSelector((state: RootState) => selectPostById(state, postId || ''))
+  const { data: post } = useGetPostQuery(postId || '')
+  const [editPost] = useEditPostMutation()
 
   const [title, setTitle] = useState(post ? post.title : '')
   const [content, setContent] = useState(post ? post.content : '')
 
-  const dispatch = useDispatch()
   const navigate = useNavigate()
 
   function handleTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
@@ -21,16 +79,10 @@ export default function EditPostForm() {
   function handleContentChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
     setContent(e.target.value)
   }
-  function handleSubmit(e: React.FormEvent<HTMLButtonElement>) {
+  async function handleSubmit(e: React.FormEvent<HTMLButtonElement>) {
     e.preventDefault()
     if (title && content) {
-      dispatch(
-        updatePost({
-          id: postId,
-          title,
-          content,
-        }),
-      )
+      await editPost({ id: postId, title, content })
       setTitle('')
       setContent('')
       navigate(`/posts/${postId}`)
